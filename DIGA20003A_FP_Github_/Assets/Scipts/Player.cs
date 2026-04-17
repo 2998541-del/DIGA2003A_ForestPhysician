6b2afa4833c411c5a79b5f205502f2f8b585bcdc
2998541-del <2998541@students.wits.ac.za>
2026-04-17 10:43:30 +0200

    crafting system

--- DIGA20003A_FP_Github_/Assets/Scipts/Player.cs
+++ DIGA20003A_FP_Github_/Assets/Scipts/Player.cs
@@ -2,15 +2,5 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Debug.Log("Pressed the arrow key");
-        }
-    }
+    public string itemName;
 }
